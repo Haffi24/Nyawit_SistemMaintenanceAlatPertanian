0308d3e147c9e5011785e946dc5c2ebc367a6d87
Haffi24 <haffisaefulloh@gmail.com>
2026-04-16 01:17:01 +0700

    ui: menambah label pilih alat dan combo box pada form maintenancealat

--- SistemMaintenanceAlatPertanian/FormMaintenanceAlat.Designer.cs
+++ SistemMaintenanceAlatPertanian/FormMaintenanceAlat.Designer.cs
@@ -28,12 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(90, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Pilih alat";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(164, 36);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(271, 21);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // FormMaintenanceAlat
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "FormMaintenanceAlat";
             this.Text = "FormMaintenanceAlat";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
--- SistemMaintenanceAlatPertanian/FormMaintenanceAlat.cs
+++ SistemMaintenanceAlatPertanian/FormMaintenanceAlat.cs
@@ -16,5 +16,10 @@ namespace SistemMaintenanceAlatPertanian
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
