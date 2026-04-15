920ef56dc745dc181fcaccf0fba14f8f3a162143
Haffi24 <haffisaefulloh@gmail.com>
2026-04-16 01:11:29 +0700

    ui : merubah nama tombol Kelola Maintenance menjadi Kelola Maintenance Alat

--- SistemMaintenanceAlatPertanian/Form1.Designer.cs
+++ SistemMaintenanceAlatPertanian/Form1.Designer.cs
@@ -48,7 +48,7 @@
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(138, 52);
             this.button2.TabIndex = 1;
-            this.button2.Text = "Kelola Maintenance";
+            this.button2.Text = "Kelola Maintenance Alat";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // button3
