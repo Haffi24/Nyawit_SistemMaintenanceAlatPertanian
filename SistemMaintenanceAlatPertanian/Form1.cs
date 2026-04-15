050baf5c0f4b85a47d932f59fdf18d3e0857b3e8
Haffi24 <haffisaefulloh@gmail.com>
2026-04-16 00:44:19 +0700

    chore: menamai ulang form1 menjadi Dashboard

--- SistemMaintenanceAlatPertanian/Form1.cs
+++ SistemMaintenanceAlatPertanian/Form1.cs
@@ -10,11 +10,16 @@ using System.Windows.Forms;
 
 namespace SistemMaintenanceAlatPertanian
 {
-    public partial class Form1: Form
+    public partial class form1: Form
     {
-        public Form1()
+        public form1()
         {
             InitializeComponent();
+        }
+
+        private void Dashboard_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
