5ab4e4b5b523e07e4c917c9cea802817a3b68e3f
Haffi24 <haffisaefulloh@gmail.com>
2026-04-16 00:52:12 +0700

    ui(dashboard): menambah buttons untuk Kelola Alat, Kelola Teknisi dan Kelola Maintenance

--- SistemMaintenanceAlatPertanian/Form1.Designer.cs
+++ SistemMaintenanceAlatPertanian/Form1.Designer.cs
@@ -28,13 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(100, 354);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(127, 52);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Kelola Alat";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(324, 354);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(138, 52);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Kelola Maintenance";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(570, 354);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(145, 52);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Kelola Teknisi";
+            this.button3.UseVisualStyleBackColor = true;
             // 
             // form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Name = "form1";
             this.Text = "Dasboard";
             this.Load += new System.EventHandler(this.Dashboard_Load);
@@ -43,6 +76,10 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
